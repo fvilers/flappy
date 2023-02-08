efe340c27ef42ebda2f41a8c91860e72897cb75c
Fabian Vilers <fabian.vilers@dev-one.com>
2023-02-08 11:46:19 +0100

    Add main menu and game over menu

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,69 @@
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    GameOver,
+}
+
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy");
+        ctx.print_centered(8, "(P)lay the game");
+        ctx.print_centered(9, "(Q)uit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quit(),
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        // TODO: implement this
+        self.mode = GameMode::GameOver;
+    }
+
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Game Over");
+        ctx.print_centered(8, "(P)lay again");
+        ctx.print_centered(9, "(Q)uit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quit(),
+                _ => {}
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!")
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::GameOver => self.game_over(ctx),
+        }
     }
 }
 
@@ -14,5 +72,5 @@ fn main() -> BError {
         .with_title("Flappy Bird")
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
